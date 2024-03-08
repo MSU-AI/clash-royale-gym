b8419d67b83dcc397d9e519f13051e7554bf05c9
Nathanael Lu <lunathanael@gmail.com>
2024-03-08 18:54:01 -0500

    revert previous + find:

--- clash_royale/__init__.py
+++ clash_royale/__init__.py
@@ -2,6 +2,6 @@ from gymnasium.envs.registration import register
 
 register(
      id="clash-royale",
-     entry_point="clash_royale.envs.clash_royale_env:ClashRoyaleEnv",
+     entry_point="clash_royale.envs:ClashRoyaleEnv",
      max_episode_steps=14400,
 )
