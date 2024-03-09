7b22fbfa97105e3fd5f2301e870cb5b73cae0c17
Owen-Cochell <owencochell@gmail.com>
2024-03-09 16:24:09 -0500

    Implemented entity attack scheduling, general entity cleanup

--- clash_royale/envs/game_engine/logic/attack.py
+++ clash_royale/envs/game_engine/logic/attack.py
@@ -8,9 +8,14 @@ We also describe what an attack does to an enemy,
 be it do damage (default) or effect done to enemy (slowness)
 """
 
-from clash_royale.envs.ngame_engine.arena import Arena
-from clash_royale.envs.ngame_engine.entities.entity import Entity
-from clash_royale.envs.ngame_engine.utils import distance
+from typing import TYPE_CHECKING
+
+from clash_royale.envs.game_engine.arena import Arena
+from clash_royale.envs.game_engine.entities.entity import Entity
+from clash_royale.envs.game_engine.utils import distance
+
+if TYPE_CHECKING:
+    from clash_royale.envs.game_engine.entities.logic_entity import LogicEntity
 
 
 class BaseAttack:
@@ -24,8 +29,10 @@ class BaseAttack:
 
     def __init__(self) -> None:
         
-        self.entity: Entity = None  # Entity we are managing
-        self.arena: Arena = None  # Arena we are apart of
+        self.entity: LogicEntity  # Entity we are managing
+        self.arena: Arena  # Arena we are apart of
+
+        self.last_attack: int = 0  # Frame of last attack
 
     def entity_distance(self, tentity: Entity) -> float:
         """
@@ -39,13 +46,44 @@ class BaseAttack:
 
         return distance(self.entity.x, self.entity.y, tentity.x, tentity.y)
 
+    def can_attack(self) -> bool:
+        """
+        Determines if this entity can attack.
+
+        We ensure the targeted entity is within our attack range,
+        and that the attack delay has been reached.
+
+        :return: True if we can attack, False if not
+        :rtype: bool
+        """
+
+        # Determine if we have a target:
+
+        if self.entity.target_ent is not None:
+
+            # Determine if our delay has passed:
+
+            if self.entity.collection.engine.scheduler.frame() > (self.entity.stats.attack_delay + self.last_attack):
+
+                # Determine if entity is within range:
+
+                if self.entity.stats.attack_range <= self.entity_distance(self.entity.target_ent):
+
+                    # Within range, return True:
+
+                    return True
+                
+        # Otherwise, return False:
+                
+        return False
+
     def attack(self):
         """
         Preforms an attack on a target, if any.
         """
 
         raise NotImplementedError("Must implement this function!")
-    
+
 
 class SingleAttack(BaseAttack):
     """
@@ -61,22 +99,10 @@ class SingleAttack(BaseAttack):
         Preforms an attack operation.
         """
 
-        # First, determine if we have a target:
+        # Determine if we can attack:
 
-        if self.entity.target is None:
+        if self.can_attack():
 
-            # No target, do nothing:
+            # Otherwise, preform an attack:
 
-            return
-
-        # Determine if target is within range:
-
-        if self.entity.stats.attack_range <= self.entity_distance(self.entity, self.entity.target):
-
-            # Not within range, do nothing
-
-            return
-
-        # Otherwise, preform an attack:
-
-        self.entity.target.stats.health -= self.entity.stats.damage
+            self.entity.target_ent.stats.health -= self.entity.stats.damage
