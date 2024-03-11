5640449d043c23d01f8389cea07f18b1179695a0
Nathanael Lu <lunathanael@gmail.com>
2024-03-11 19:12:03 -0400

    shorten line length, rename __init__.py

--- clash_royale/envs/game_engine/logic/__init.py
+++ clash_royale/envs/game_engine/logic/__init.py
@@ -5,7 +5,7 @@ Logic components are self-contained classes that describes behavior.
 These classes can be utilized in entities that support them.
 The components can be broken down into these pieces:
 
-- Movement - How does this entity move? Should take into consideration movement speed and flying/ground
+- Movement - How does this entity move? Consider movement speed and flying/ground
 - Targeting - How are entity targets selected? Should consider sight range and entity targets
 - Attack - How does this entity attack? Damage? Single/splash?
 """
